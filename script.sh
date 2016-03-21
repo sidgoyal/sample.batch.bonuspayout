27b8709ec9905fa3288c23bce97f5439d062a36e
Siddharth Goyal <sidgoyal@users.noreply.github.com>
2016-03-20 23:09:03 -0400

    Update script.sh

--- script.sh
+++ script.sh
@@ -7,7 +7,7 @@ cp $BONUSPAYOUT_DIR/batch-bonuspayout-wlpcfg/* .
 export WLP_USER_DIR=$WLP_DIR/usr/
 cd servers/BonusPayout
 
-../../..//bin/batchManager submit --batchManager=localhost:9443 --trustSslCertificates --user=bob --password=bobpwd --applicationName=BonusPayout --jobXMLName=BonusPayoutJob --jobParameter="numRecords=100" --wait --pollingInterval_s=1
+../../../bin/batchManager submit --batchManager=localhost:9443 --trustSslCertificates --user=bob --password=bobpwd --applicationName=BonusPayout --jobXMLName=BonusPayoutJob --jobParameter="numRecords=100" --wait --pollingInterval_s=1
 
 ##CURL REQUEST TO READ THE DB
 curl -XPOST 'https://79cfd3d0-8e3c-416a-83ef-362ebb4745ec-bluemix.cloudant.com/cloudant-bonuspayout/_find'  -H 'Content-Type: application/json' -d '{"selector":{"_id":{"$gt":0}},"fields":["_id","accountNumber","balance","instanceId"],"sort":[{"_id":"asc"}]}'
